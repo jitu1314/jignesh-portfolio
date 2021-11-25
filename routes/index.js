5e82b39c59091586b4672e10e8c30736cd089395
jignesh <jitukumavat234@gmail.com>
2021-11-24 21:10:19 -0500

    Assignment five

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+   res.render('index');
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'ABout' });
+  res.render('about', { title: 'About Page' });
 });
 
-router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact' });
-});
-
-router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects' });
+router.get('/project', function(req, res, next) {
+  res.render('project', { title: 'Project Page' });
 });
 
 module.exports = router;
